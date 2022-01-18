f58d4228c4939048d809dd96a510a54a32dbe64f
ddecataldo <davidedecataldo92@gmail.com>
2022-01-18 18:06:45 +0100

    Minor Fix

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -1,19 +1,20 @@
 <h1>Aggiornamenti dati fumetto "{{ $comic->title }}"</h1>
-<form action="{{route("comics.store")}}" method="post">
+<form action="{{route("comics.update",  $comic->id)}}" method="post">
     @csrf
+    @method("PUT")
     <label for="inputTitle">Title</label>
-    <input type="text" name="title" id="inputTitle">
+    <input type="text" name="title" id="inputTitle" value="{{old("title") ?? $comic->title}}">
     <label for="inputDescription">Description</label>
-    <input type="text" name="description" id="inputDescription">
+    <input type="text" name="description" id="inputDescription" value="{{old("description") ?? $comic->description}}">
     <label for="inputThumb">Thumb</label>
-    <input type="text" name="thumb" id="inputThumb">
+    <input type="text" name="thumb" id="inputThumb" value="{{old("thumb") ?? $comic->thumb}}">
     <label for="inputPrice">Price</label>
-    <input type="number" name="price" id="inputPrice">
+    <input type="number" name="price" id="inputPrice" value="{{old("price") ?? $comic->price}}">
     <label for="inputSeries">Series</label>
-    <input type="text" name="series" id="inputSeries">
+    <input type="text" name="series" id="inputSeries" value="{{old("series") ?? $comic->series}}">
     <label for="inputSaleDate">Date</label>
-    <input type="date" name="sale_date" id="inputSaleDate">
+    <input type="date" name="sale_date" id="inputSaleDate" value="{{old("sale_date") ?? $comic->sale_date}}">
     <label for="inputType">Type</label>
-    <input type="text" name="type" id="inputType">
+    <input type="text" name="type" id="inputType" value="{{old("type") ?? $comic->type}}">
     <button type="submit">Confirm</button>
 </form>
