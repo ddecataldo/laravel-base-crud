a67f5f5b81fa92dbcc1a8dedecb16caf587c462b
ddecataldo <davidedecataldo92@gmail.com>
2022-01-18 18:08:09 +0100

    Add button delete

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -5,6 +5,11 @@
         {{ $singleComics->title }} - 
         <a href="{{ route('comics.show', $singleComics->id) }}"> Vedi i dettagli </a> -
         <a href="{{ route('comics.edit', $singleComics->id) }}"> Modifica </a>
+        <form action="{{ route('comics.destroy', $singleComics->id) }}" method="post">
+            @csrf
+            @method('delete')
+            <button type="submit">Delete</button>
+        </form>
     </li>
 @endforeach
 </ul>
