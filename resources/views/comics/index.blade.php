fdeb5517a846637e01a23a01b2779ff283b8c659
ddecataldo <davidedecataldo92@gmail.com>
2022-01-18 16:45:12 +0100

    Show link comics.show and comics.edit in the page

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,6 +1,10 @@
 <h1>Lista Fumetti</h1>
 <ul>
-@foreach ($comics as $singleComics)
-    <li>{{ $singleComics->title }}</li>
+@foreach ($comic as $singleComics)
+    <li>
+        {{ $singleComics->title }} - 
+        <a href="{{ route('comics.show', $singleComics->id) }}"> Vedi i dettagli </a> -
+        <a href="{{ route('comics.edit', $singleComics->id) }}"> Modifica </a>
+    </li>
 @endforeach
 </ul>
